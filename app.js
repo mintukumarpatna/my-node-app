d027aee89d5c987ff921ee1023044b1e0706a6cb
Mintu Kumar <mintu.kumar@thoughtsol.in>
2026-04-29 11:07:07 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('CI/CD Pipeline Working 🚀');
+});
+
+// ✅ FIX HERE
+const server = app.listen(3000, '0.0.0.0', () => {
+  console.log('Server running on port 3000');
+});
+
+// graceful shutdown
+process.on('SIGTERM', () => {
+  server.close(() => {
+    process.exit(0);
+  });
+});
