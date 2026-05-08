248e131db952d669426e26cf190a452f8751f675
Mintu Kumar <mintu.kumar@thoughtsol.in>
2026-05-08 16:37:25 +0530

    love

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.get('*', (req, res) => {
 // Start server
 const server = app.listen(PORT, HOST, () => {
   console.log('====================================');
-  console.log('🚀 I LOVE YOU!');
+  console.log(' I LOVE YOU!');
   console.log(`🌐 URL: http://${HOST}:${PORT}`);
   console.log(`🖥️ Hostname: ${os.hostname()}`);
   console.log(`⏱️ Time: ${new Date().toLocaleString()}`);
